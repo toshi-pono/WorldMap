3dc982069354007cb21866f561b3d6ef85f1c45a
toshi/pono000 <toshi.pono.h@gmail.com>
2020-09-16 22:01:27 +0900

    fix(あとでちゃんとする）

--- html/setting/script.js
+++ html/setting/script.js
@@ -4,6 +4,114 @@ let pingPongTimer = null;
 let isLoad = true;
 let app;
 
+// データ
+// TODO:jsonで読み込むようにする
+// TODO:データをちゃんと整理する
+const assetsUrl = "../assets/";
+let textureList = [
+  {
+    name: "building1",
+    url: assetsUrl + "building1.PNG",
+    width: 66,
+    height: 100,
+  },
+  {
+    name: "building2",
+    url: assetsUrl + "building2.PNG",
+    width: 66,
+    height: 100,
+  },
+  {
+    name: "building3",
+    url: assetsUrl + "building3.PNG",
+    width: 66,
+    height: 100,
+  },
+  {
+    name: "building4",
+    url: assetsUrl + "building4.PNG",
+    width: 100,
+    height: 100,
+  },
+  {
+    name: "home1",
+    url: assetsUrl + "home1.PNG",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "home2",
+    url: assetsUrl + "home2.PNG",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "home3",
+    url: assetsUrl + "home3.PNG",
+    width: 70,
+    height: 70,
+  },
+  {
+    name: "tower1",
+    url: assetsUrl + "tower1.PNG",
+    width: 100,
+    height: 200,
+  },
+  {
+    name: "tree1",
+    url: assetsUrl + "tree1.PNG",
+    width: 70,
+    height: 70,
+  },
+];
+
+let flyTextureList = [
+  {
+    name: "fly1",
+    url: assetsUrl + "fly1.PNG",
+    width: 100,
+    height: 75,
+    speed: 30,
+  },
+  {
+    name: "fly2",
+    url: assetsUrl + "fly2.PNG",
+    width: 100,
+    height: 75,
+    speed: 10,
+  },
+  {
+    name: "fly3",
+    url: assetsUrl + "fly3.PNG",
+    width: 100,
+    height: 75,
+    speed: 8,
+  },
+  {
+    name: "cloud",
+    url: assetsUrl + "cloud.PNG",
+    width: 100,
+    height: 75,
+    speed: 2,
+  },
+];
+
+let carTextureList = [
+  {
+    name: "car1",
+    url: assetsUrl + "buta.png",
+    width: 70,
+    height: 70,
+    speed: 1,
+  },
+];
+
+function handleTouchMove(event) {
+  event.preventDefault();
+}
+//スクロール禁止
+document.addEventListener("touchmove", handleTouchMove, { passive: false });
+
 const SCREEN_SETTING = {
   width: 750,
   height: 1125,
